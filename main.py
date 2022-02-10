0574e6aa5f05e079306e70a1d236587b670bbc8c
Manolo <manuelgmaroto@gmail.com>
2022-02-10 12:31:15 +0100

    Created presentation and question

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello')
+print('***********************************************************')
+print('')
+print('        WELCOME TO WHO WONTS TO BE MILLIONAIRE!!!!!         ')
+print('')
+print('***********************************************************')
+print('')
+print('')
+
+contestants = 0
+
+contestants = input('How many contestants are there? ')
+print('There are ' + str(contestants) + ' contestants.')
+
